ab82ffba6973562087761e19458360aa4fa66b48
h.nomura <h.nomura0504@gmail.com>
2020-03-25 00:43:04 +0900

    cheerio, fsをパッケージに追加、本番サーバー用のpm2設定ファイルを追加

---
+++ pm2.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps : [
+        {
+        name: "covid19",
+        script: "yarn start",
+        cwd : '/root/website/covid19'
+        }
+    ]
+}
--- pm2_config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps : [{
-    name: 'covid',
-    exec_mode : 'cluster',
-    instances: 0,
-    cwd: '/root/website/covid19',
-    script: '/usr/bin/yarn start',
-  }]
-}
