a353492f1ddf313ba06b4c88648ba0b1cdfbe371
h.nomura <h.nomura0504@gmail.com>
2020-03-27 19:59:51 +0900

    陽性患者の詳細テーブルに患者詳細カラムの追加

--- utils/formatTable.ts
+++ utils/formatTable.ts
@@ -5,7 +5,7 @@ const headers = [
   { text: '居住地', value: '居住地' },
   { text: '年代', value: '年代' },
   { text: '性別', value: '性別' },
-  { text: '退院※', value: '退院', align: 'center' }
+  { text: '備考', value: '備考' }
 ]
 
 type DataType = {
@@ -13,7 +13,7 @@ type DataType = {
   居住地: string | null
   年代: string | null
   性別: '男性' | '女性' | string
-  退院: '◯' | null
+  備考: string | null
   [key: string]: any
 }
 
@@ -22,7 +22,7 @@ type TableDataType = {
   居住地: DataType['居住地']
   年代: DataType['年代']
   性別: DataType['性別'] | '不明'
-  退院: DataType['退院']
+  備考: DataType['備考']
 }
 
 type TableDateType = {
@@ -46,7 +46,7 @@ export default (data: DataType[]) => {
       居住地: d['居住地'] ?? '不明',
       年代: d['年代'] ?? '不明',
       性別: d['性別'] ?? '不明',
-      退院: d['退院']
+      備考: d['備考']
     }
     tableDate.datasets.push(TableRow)
   })
