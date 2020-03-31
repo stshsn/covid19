4e4fe1f5b4a64f83a109294e2cdf532a0f4d537f
h.nomura <h.nomura0504@gmail.com>
2020-03-31 12:28:59 +0900

    緊急エラー対応

--- components/cards/YoutubeCard.vue
+++ components/cards/YoutubeCard.vue
@@ -2,9 +2,9 @@
   <v-col cols="12" class="DataCard">
     <v-card>
       <v-card-title>福井県のYouTube公開動画</v-card-title>
-      <v-card-subtitle>公開日時: {{ publishedAt }}</v-card-subtitle>
+      <!-- <v-card-subtitle>公開日時: {{ publishedAt }}</v-card-subtitle> -->
       <v-card-text>
-        <youtube :video-id="videoId" />
+        <!-- <youtube :video-id="videoId" /> -->
       </v-card-text>
     </v-card>
   </v-col>
@@ -13,27 +13,27 @@
 <script>
 import Vue from 'vue'
 import VueYoutube from 'vue-youtube'
-import videolist from '../../covid19_fukui/youtubeVideolist.json'
+// import videolist from '../../covid19_fukui/youtubeVideolist.json'
 
 Vue.use(VueYoutube)
 
 export default {
-  data: () => ({
-    videoId: '',
-    publishedAt: ''
-  }),
+//   data: () => ({
+//     videoId: '',
+//     publishedAt: ''
+//   }),
 
-  created () {
-    this.getLatestVideoId()
-  },
+//   created () {
+//     this.getLatestVideoId()
+//   },
 
-  methods: {
-    async getLatestVideoId(userName) {
-      this.videoId = videolist[0].snippet.resourceId.videoId
-      this.publishedAt = new Date(videolist[0].snippet.publishedAt).toLocaleString()
-    }
-  }
-}
+//   methods: {
+//     async getLatestVideoId(userName) {
+//       this.videoId = videolist[0].snippet.resourceId.videoId
+//       this.publishedAt = new Date(videolist[0].snippet.publishedAt).toLocaleString()
+//     }
+//   }
+// }
 </script>
 
 <style>
