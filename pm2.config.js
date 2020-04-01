f0541f92f4551b752775d57256cc1d70c83899b4
h.nomura <h.nomura0504@gmail.com>
2020-04-02 00:54:29 +0900

    dev用と本番用のpm2設定を追加

--- pm2.config.js
+++ pm2.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     apps : [
         {
+            name: "covid19",
+            script: "yarn start",
+            cwd : '/root/website/covid19'
+        },
+        {
             name: "covid19_dev",
             script: "yarn dev:server",
             cwd : '/root/website/covid19_dev'
