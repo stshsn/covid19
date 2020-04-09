6192d569691215ed8886db7f8bfe770503f75a46
FooQoo <horseshoes.buzz.17@gmail.com>
2020-04-10 01:03:46 +0900

    mv csv2json

--- covid19_fukui/csv2json.js
+++ covid19_fukui/csv2json.js
@@ -324,3 +324,58 @@ const dateFormat = {
 }
 
 main()
+
+const main2 = () => {
+  const getFukuiShimbun = () => {
+    const axios = require('axios')
+    const moment = require('moment-timezone')
+    const xml2js = require('xml2js')
+    moment.tz.setDefault("Asia/Tokyo");
+  
+    return new Promise(async (resolve, reject) => {
+      try {
+        const res = await axios.get('https://www.fukuishimbun.co.jp/list/feed/rss')
+        const xml = res.data
+        const json = {
+          timestamp: moment().unix(),
+          info: null
+        }
+        xml2js.parseString(xml, (_, xmlres) => {
+          json.info = xmlres.rss.channel[0].item
+          .map((i) => {
+              return {
+                title: i.title[0],
+                link: i.link[0],
+                published_at: moment(i.pubDate[0]).format('YYYY/MM/DD HH:mm')
+              }
+            })
+        })
+        resolve(json)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  
+  const storeFukuiShimbun = async () => {
+    try {
+      const info = await getFukuiShimbun()
+      const fs = require('fs')
+      fs.writeFile(
+        '../data/fukuishimbun.json',
+        JSON.stringify(info, null, '    '),
+        function(err) {
+          if (err) {
+            console.log(err)
+          }
+      }
+    )
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  storeFukuiShimbun()
+}
+
+main2()
