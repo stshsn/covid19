aa93b3d76d045862d8ca9cdc273437b2789ee5da
HyodaKazuaki <kazuaki@ex-t.jp>
2020-04-12 19:19:37 +0900

    最新のお知らせ(福井県内)カードをトップページから除去 サイドバーにはリンクが残っている

--- pages/index.vue
+++ pages/index.vue
@@ -14,10 +14,8 @@
     </div>
     <breaking-news class="mb-4" :items="BreakingItems" />
     <fukui-paper-news class="mb-4" />
-    <!--
-    <fukui-news class="mb-4" />
-    -->
-    <whats-new class="mb-4" :items="newsItems" />
+    <!-- <fukui-news class="mb-4" />
+    <whats-new class="mb-4" :items="newsItems" /> -->
     <whats-new-japan class="mb-4" :items="japanItems" />
     <static-info
       class="mb-4"
@@ -46,7 +44,7 @@ import { MetaInfo } from 'vue-meta'
 import PageHeader from '@/components/PageHeader.vue'
 // 速報
 import BreakingNews from '@/components/BreakingNews.vue'
-import WhatsNew from '@/components/WhatsNew.vue'
+// import WhatsNew from '@/components/WhatsNew.vue'
 import WhatsNewJapan from '@/components/WhatsNewJapan.vue'
 import FukuiPaperNews from '@/components/FukuiPaperNews.vue'
 // import FukuiNews from '@/components/FukuiNews.vue'
@@ -96,7 +94,7 @@ export default Vue.extend({
     BreakingNews,
     // FukuiNews,
     FukuiPaperNews,
-    WhatsNew,
+    // WhatsNew,
     WhatsNewJapan,
     StaticInfo,
     ConfirmedCasesNumberCard,
