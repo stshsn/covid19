7638a1d8a96a9057ec385922714e3e0e5b533385
h.nomura <h.nomura0504@gmail.com>
2020-04-19 21:29:17 +0900

    福井県のテーマカラーに変更

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,4 +1,4 @@
-export const single: string = '#006400'
-export const double: string[] = ['#006400', '#1B75BC']
-export const triple: string[] = ['#00441B', '#006400', '#1B75BC']
-export const quadruple: string[] = ['#00441B', '#006400', '#1B75BC', '#505B00']
+export const single: string = '#003580'
+export const double: string[] = ['#003580', '#006aff']
+export const triple: string[] = ['#003580', '#006aff', '#66a6ff']
+export const quadruple: string[] = ['#003580', '#006aff', '#66a6ff', '#99c3ff']
